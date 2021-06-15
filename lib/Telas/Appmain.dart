c44c639d7c8e9365c1ac60f1c33cfd1edf05ec35
Malkai <omalkai14@gmail.com>
2021-06-15 20:37:29 -0300

    Continuação da alteração da UI das telas

--- lib/Telas/Appmain.dart
+++ lib/Telas/Appmain.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/componentes/textofield.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class Appmain extends StatelessWidget {
   final int conta = 0;
@@ -11,6 +13,30 @@ class Appmain extends StatelessWidget {
         children: [
           Container(
             color: Colors.black,
+            child: FlutterMap(
+              options: MapOptions(
+                center: LatLng(51.5, -0.09),
+                zoom: 13.0,
+              ),
+              layers: [
+                TileLayerOptions(
+                    urlTemplate:
+                        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                    subdomains: ['a', 'b', 'c']),
+                MarkerLayerOptions(
+                  markers: [
+                    Marker(
+                      width: 80.0,
+                      height: 80.0,
+                      point: LatLng(51.5, -0.09),
+                      builder: (ctx) => Container(
+                        child: FlutterLogo(),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
           Positioned(
             top: 30,
